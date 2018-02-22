dee3fd2433515076df93a71bf9a268ba736ace5c
sirkibsirkib <christopher.esterhuyse@gmail.com>
2018-02-22 16:48:45 +0100

    more

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
 use std::fmt;
 
+#[derive(Eq, PartialEq, Copy, Clone)]
+pub enum FormulaType {
+	Letter, Negation, Conjunction, Disjunction, MDiamond, MBox, Implication, None
+}
+impl FormulaType {
+	pub fn bind_strength(&self) -> u8 {
+		use FormulaType::*;
+		match self {
+			&None => 99,
+			&Letter => 99, 
+			&Negation => 3,
+			&Conjunction => 2,
+			&Disjunction => 2,
+			&MDiamond => 3,
+			&MBox => 3,
+			&Implication => 1,
+		}
+	}
+}
+
+
 #[derive(Clone)]
 pub enum Formula {
-	Letter(u32),
+	Letter(u8),
 	Negation(Box<Formula>),
 	Conjunction(Box<Formula>, Box<Formula>),
 	Disjunction(Box<Formula>, Box<Formula>),
@@ -12,19 +33,52 @@ pub enum Formula {
 	MBox(Box<Formula>),
 	Implication(Box<Formula>, Box<Formula>),
 }
-impl fmt::Debug for Formula {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	
+impl Formula {
+	fn get_type(&self) -> FormulaType {
 		use Formula::*;
 		match self {
-			&Letter(x) => 					write!(f, "{:?}", &x),
-			&Negation(ref x) => 			write!(f, "-{:?}", &x),
-			&Conjunction(ref x, ref y) => 	write!(f, "({:?}&{:?})", &x, &y),
-			&Disjunction(ref x, ref y) => 	write!(f, "({:?}V{:?})", &x, &y),
-			&MDiamond(ref x) => 			write!(f, "<>{:?}", &x),
-			&MBox(ref x) => 				write!(f, "[]{:?}", &x),
-			&Implication(ref x, ref y) => 	write!(f, "({:?}->{:?})", &x, &y),
+			&Letter(_) => 			FormulaType::Letter,
+			&Negation(_) => 		FormulaType::Negation,
+			&Conjunction(_,_) => 	FormulaType::Conjunction,
+			&Disjunction(_,_) => 	FormulaType::Disjunction,
+			&MDiamond(_) => 		FormulaType::MDiamond,
+			&MBox(_) => 			FormulaType::MBox,
+			&Implication(_,_) => 		FormulaType::Implication,
 		}
+	}
+
+	fn need_parens(my_type: FormulaType, their_type: FormulaType) -> bool {
+		if my_type == their_type {
+			return false;
+		} else if their_type == FormulaType::None {
+			return false;
+		}
+		their_type.bind_strength() > my_type.bind_strength()
+	}
+
+    fn repr(&self, f: &mut String, inside_type: FormulaType) {
+    	let my_type = self.get_type();
+		let parens = Formula::need_parens(my_type, inside_type);
+		if parens {f.push('(');}
+		use Formula::*;
+		match self {
+			&Letter(x) => 					{f.push_str(&format!("{}", (x + 'a' as u8) as char));},
+			&Negation(ref x) => 			{f.push('-'); x.repr(f, my_type);},
+			&Conjunction(ref x, ref y) => 	{x.repr(f, my_type); f.push('&'); y.repr(f, my_type);},
+			&Disjunction(ref x, ref y) => 	{x.repr(f, my_type); f.push('V'); y.repr(f, my_type);},
+			&MDiamond(ref x) => 			{f.push('<'); f.push('>'); x.repr(f, my_type);},
+			&MBox(ref x) => 				{f.push('['); f.push(']'); x.repr(f, my_type);},
+			&Implication(ref x, ref y) => 	{x.repr(f, my_type); f.push('-'); f.push('>'); y.repr(f, my_type);},
+		};
+		if parens {f.push(')');}
+    }
+}
+
+impl fmt::Debug for Formula {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let mut s = String::new();
+		self.repr(&mut s, FormulaType::None);
+		write!(f, "{}", &s)
     }
 }
 
@@ -66,7 +120,7 @@ impl MetaImpl {
 	}
 	
 	pub fn validity(&self) -> Validity {
-		let mut lefts: Vec<u32> = vec![];
+		let mut lefts: Vec<u8> = vec![];
 		for l in self.left.iter() {
 			if let &Formula::Letter(x) = l {
 				lefts.push(x);
@@ -92,12 +146,30 @@ impl MetaImpl {
 	}
 }
 
+// pub fn parse(text: &str) -> Option<Formula> {
+// 	use Formula::*;
+// 	for i in 0..text.len() {
+// 		if i == '-' {
+// 			return Formula::Negation(Box::new(parse()))
+// 		}
+// 	}
+// }
+
 #[cfg(test)]
 mod tests {
+	use super::*;
     #[test]
     fn it_works() {
 		use Formula::*;
-		let x = Implication(Box::new(Letter(0)), Box::new(Letter(1)));
+		let x = Implication(
+			Box::new(Disjunction(
+				Box::new(Letter(0)),
+				Box::new(MBox(Box::new(Letter(1)))),
+			)),
+			Box::new(Letter(1)),
+		);
+		println!("{:?}", &x);
+		let x = preprocess(x);
 		println!("{:?}", &x);
     }
 }
